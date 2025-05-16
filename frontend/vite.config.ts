25ab9a89a2c6fe14e724dafd3cb78ddecb56818a
Trailblazer-dev <richvictor830@gmail.com>
2025-05-16 11:58:41 +0300

    for deployment4 tampered with tailwindcss config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from "@tailwindcss/vite"
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -18,15 +17,13 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: false,
     minify: true,
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          router: ['react-router-dom']
-        }
-      }
-    }
+        manualChunks: undefined,
+      },
+    },
   },
 });
