d50ca2b0936fbd5d3e8513eb3daf377284c0ec1a
Trailblazer-dev <richvictor830@gmail.com>
2025-05-17 16:58:10 +0300

    wooh

--- carhire/rentals/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, CarModel, Booking, PaymentModel
-admin.site.register(User)
-admin.site.register(CarModel)
-admin.site.register(Booking)
-admin.site.register(PaymentModel)
-# Register your models here.
